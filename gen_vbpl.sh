01afdab58d97ae472b8814c3b3a82c63baf703d5
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-12-11 14:21:57 +0700

    Add vbpl gen support

---
+++ gen_vbpl.sh
@@ -0,0 +1,6 @@
+python3 main.py --chars_file data/chars/vie.txt \
+                --fonts_list data/fonts_list/vie.txt \
+                --corpus_mode vbpl \
+                --length 12 \
+                --num_img 100 \
+                --strict
