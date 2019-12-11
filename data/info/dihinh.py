ce19ad1abb9fbd14788fbf3ce827250a53e1c48b
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-12-11 14:20:26 +0700

    fuck

--- data/info/dihinh.py
+++
@@ -1,4 +0,0 @@
-loai_di_hinh = ['Sẹo chấm', 'Nốt ruồi', 'Sẹo thẳng', 'Sẹo tròn', 'Sẹo cong', "N.r"]
-tu_noi_khoang_cach = ['c:', 'c.', 'd.', 't.', 'cách']
-tu_noi_vi_tri = ['trên', 'dưới', 'trái', 'phải', 'sau', 'trước']
-vi_tri = ['đuôi mày', 'mép', 'mí mắt', 'đuôi mắt', 'đầu mày', 'đầu mắt', 'cánh mũi', 'đuôi mi']
