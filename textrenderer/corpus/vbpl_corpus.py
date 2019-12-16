7928cbdd5c7617f95dc61cfd7c83fbc9ee038e12
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-12-17 01:38:30 +0700

    Changed corpus

--- textrenderer/corpus/vbpl_corpus.py
+++ textrenderer/corpus/vbpl_corpus.py
@@ -2,7 +2,7 @@ from textrenderer.corpus.corpus import Corpus
 
 class VBPLCorpus(Corpus):
     def load(self):
-        self.all_lines = open('data/corpus/fixed_corpus.txt','r').read().split('\n')
+        self.all_lines = open('data/corpus/extracted_300k.txt','r').read().split('\n')
 
     def get_sample(self, index):
         return self.all_lines[index]
