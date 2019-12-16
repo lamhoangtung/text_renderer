9e8008d70f2ccb457ee2fc8f44ce0f7a5b11e24b
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-12-17 01:38:09 +0700

    Better config for barebone gen

--- gen_vbpl.sh
+++ gen_vbpl.sh
@@ -2,10 +2,14 @@ python3 main.py --chars_file data/chars/vie.txt \
                 --fonts_list data/fonts_list/vie.txt \
                 --config_file configs/vbpl.yaml \
                 --bg_dir data/bg/ \
-                --length 150 \
                 --corpus_mode vbpl \
-                --length 12 \
-                --num_img 100 \
-                --tag clean \
+                --length 150 \
+                --num_img 50 \
+                --img_height 96 \
+                --tag randomize_400k \
                 --strict \
-                --clip_max_chars
+                --clip_max_chars \
+                # --num_processes 2
+                # --debug \
+                # --corpus_mode random \
+                # --length 150 \
