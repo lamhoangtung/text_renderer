5f5bbc4614f20bf9a11e4aaadca28c0a44fb215e
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-12-24 11:43:29 +0700

    Randomize each word when sample from english corpus

--- textrenderer/corpus/eng_corpus.py
+++ textrenderer/corpus/eng_corpus.py
@@ -1,6 +1,6 @@
 from textrenderer.corpus.corpus import Corpus
 import numpy as np
-
+import random
 
 class EngCorpus(Corpus):
     """
@@ -26,7 +26,14 @@ class EngCorpus(Corpus):
             print("Word count {}".format(len(self.corpus)))
 
     def get_sample(self, img_index):
-        start = np.random.randint(0, len(self.corpus) - self.length)
-        words = self.corpus[start:start + self.length]
+        # start = np.random.randint(0, len(self.corpus) - self.length)
+        # words = self.corpus[start:start + self.length]
+        words = random.sample(self.corpus, k=random.randint(2, self.length))
+        for index, word in enumerate(words):
+            mode = int(np.random.choice(3, 1, p=[0.65, 0.25, 0.1])) # 0: lowercase, 1: firstupcase, 2: allupcase
+            if mode == 1:
+                words[index] = word.title()
+            elif mode == 2:
+                words[index] = word.upper()
         word = ' '.join(words)
         return word
