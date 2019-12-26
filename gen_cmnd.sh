cb2292b03e9e052a250e4b9a452f69ed0cb41b77
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-12-26 19:59:28 +0700

    Add new gen script

--- gen_cmnd.sh
+++ gen_cmnd.sh
@@ -1,12 +1,12 @@
 python3 main.py --chars_file data/chars/vie.txt \
-                --fonts_list data/fonts_list/vie.txt \
-                --config_file configs/vbpl.yaml \
+                --fonts_list data/fonts_list/eng_vbpl.txt \
+                --config_file configs/cmnd.yaml \
                 --bg_dir data/bg/ \
-                --corpus_mode vbpl \
+                --corpus_mode cmnd \
                 --length 150 \
                 --num_img 50 \
                 --img_height 96 \
-                --tag randomize_400k \
+                --tag randomize_cmnd_200k \
                 --strict \
                 --clip_max_chars \
                 --num_processes 2
