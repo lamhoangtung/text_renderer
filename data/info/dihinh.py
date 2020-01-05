bc335182718c2daae39ad8416f36e2f65327ba83
lamhoangtung <lamhoangtung.vz@gmail.com>
2020-01-05 23:06:22 +0700

    Add id card corpus suplement data

---
+++ data/info/dihinh.py
@@ -0,0 +1,4 @@
+loai_di_hinh = ['Sẹo chấm', 'Nốt ruồi', 'Sẹo thẳng', 'Sẹo tròn', 'Sẹo cong', "N.r", 'Tàn nhan']
+tu_noi_khoang_cach = ['c;', 'c.', 'd.', 't.', 'cách', 'CO,', 'c,', 'C.', 'ở']
+tu_noi_vi_tri = ['trên', 'dưới', 'trái', 'phải', 'sau', 'trước', 'dưới trước', 'trên trước', 'trên sau', 'dưới sau', 'ngay']
+vi_tri = ['đuôi mày', 'mép', 'mí mắt', 'đuôi mắt', 'đầu mày', 'đầu mắt', 'cánh mũi', 'đuôi mi', 'đuôi lông mày', 'mày']
