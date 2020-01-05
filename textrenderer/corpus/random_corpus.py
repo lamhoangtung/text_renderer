24003c12ba1da711db1200fd41302c15167d6d04
lamhoangtung <lamhoangtung.vz@gmail.com>
2020-01-05 23:05:14 +0700

    Better random corpus

--- textrenderer/corpus/random_corpus.py
+++ textrenderer/corpus/random_corpus.py
@@ -12,8 +12,20 @@ class RandomCorpus(Corpus):
         pass
 
     def get_sample(self, img_index):
-        word = ''
-        for _ in range(self.length):
-            word += random.choice(self.charsets)
-        return word
+        # word = ''
+        # for _ in range(self.length):
+        #     word += random.choice(self.charsets)
+        # return word
+
+        num_words = random.randint(3, 20)
+        text = []
+        for _ in range(num_words):
+            word = ''
+            num_chars = random.randint(1, 9)
+            for _ in range(num_chars):
+                word += random.choice(self.charsets)
+            text.append(word)
+        text = ' '.join(text)
+        # print(text)
+        return text
 
