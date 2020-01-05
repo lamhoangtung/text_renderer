c45898278d2187db905f8332b9b9042ec6cb6957
lamhoangtung <lamhoangtung.vz@gmail.com>
2020-01-05 23:04:37 +0700

    Move gen script

---
+++ script/gen_general.sh
@@ -0,0 +1,16 @@
+
+python3 main.py --chars_file data/chars/vie_general.txt \
+                --fonts_list data/fonts_list/vie.txt \
+                --config_file configs/general.yaml \
+                --bg_dir data/bg/ \
+                --corpus_mode random \
+                --length 150 \
+                --num_img 50 \
+                --img_height 96 \
+                --tag randomize_general_1m \
+                --strict \
+                --clip_max_chars \
+                --num_processes 2
+                # --debug \
+                # --corpus_mode random \
+                # --length 150 \
