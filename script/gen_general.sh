fa382b0e7f83c3b584b7219a92cafcc69943f0e5
lamhoangtung <lamhoangtung.vz@gmail.com>
2020-01-25 21:55:41 +0700

    Better general generator

--- script/gen_general.sh
+++ script/gen_general.sh
@@ -5,9 +5,9 @@ python3 main.py --chars_file data/chars/vie_general.txt \
                 --bg_dir data/bg/ \
                 --corpus_mode random \
                 --length 150 \
-                --num_img 50 \
+                --num_img 200 \
                 --img_height 96 \
-                --tag randomize_general_1m \
+                --tag randomize_general_1m_2 \
                 --strict \
                 --clip_max_chars \
                 --num_processes 2
